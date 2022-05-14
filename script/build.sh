18949d831a481ae5e86c96119a5c04270ae13e06
Jia <angersax@sina.com>
2022-05-14 12:56:32 +0800

    refactor(script): remove echo from script

--- script/build.sh
+++ script/build.sh
@@ -9,7 +9,6 @@ filepath="plugin"
 for item in "$filepath"/*.go; do
   buf=${item%.go}
   name=${buf##*/}
-  echo $name
   CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags "$ldflags" -o plugin/score-"$name" plugin/"$name".go
   upx plugin/score-"$name"
 done
