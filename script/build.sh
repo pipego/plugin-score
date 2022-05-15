aa9168fe0637332cee0657569c26884d431bbe0a
Jia <angersax@sina.com>
2022-05-15 15:25:18 +0800

    refactor(build): add support to build with GOARCH and GOOS

--- script/build.sh
+++ script/build.sh
@@ -9,11 +9,13 @@ filepath="plugin"
 for item in "$filepath"/*.go; do
   buf=${item%.go}
   name=${buf##*/}
-  CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags "$ldflags" -o plugin/score-"$name" plugin/"$name".go
+  # go tool dist list
+  CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=$(go env GOOS) go build -ldflags "$ldflags" -o plugin/score-"$name" plugin/"$name".go
   upx plugin/score-"$name"
 done
 
 # Test
 target="plugin-score-test"
-CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -ldflags "$ldflags" -o $target main.go
+# go tool dist list
+CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=$(go env GOOS) go build -ldflags "$ldflags" -o $target main.go
 upx $target
